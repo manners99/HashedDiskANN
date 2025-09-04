2ee5170f1bbc47fdbbd5a48078551666d4b9b0f9
Joshua Manners <jman988@aucklanduni.ac.nz>
2025-09-04 17:35:42 +1200

    updated LSH.cpp with complete untested code

--- src/lsh.cpp
+++ src/lsh.cpp
@@ -5,7 +5,8 @@
 #include <tuple>
 #include <functional>
 #include <set>
-#include <Eigen/Dense>
+#include <eigen3/Eigen/Dense>
+//use "sudo apt-get install libeigen3-dev" to intall Eigen library
 
 //Hash key type is vector<int> representing a binary hash
 using HashKey = std::vector<int>;
@@ -40,5 +41,86 @@ namespace diskann
         //Hyperplanes: One matix per table (numHashes * dimension)
         std::vector<Eigen::MatrixXd> hyperplanes;
 
+        std::mt19937 gen;
+        std::normal_distribution<double> dist;
+
+    public:
+        LSH(int numTables, int numHashes, int dimension)
+            : numTables(numTables), numHashes(numHashes), dimension(dimension), tables(numTables), gen(std::random_device{}()), dist(0.0, 1.0)
+            {
+                //Generate the random hyperplanes
+                for (int i = 0; i < numTables; i++) {
+                    Eigen::MatrixXd hp(numHashes, dimension);
+                    for (int j = 0; j < numHashes; j++ ) {
+                        for (int k = 0; k < dimension; k++) {
+                            hp(j, k) = dist(gen);
+                        }
+                    }
+                    hyperplanes.push_back(hp);
+                }
+
+                std::cout << "[INIT] Created LSH with " << numTables
+                << " tables, " << numHashes
+                << " hashes per table, dimension " << dimension << std::endl;
+            }
+
+            HashKey hashFunction(const Eigen::VectorXd& point, const Eigen::MatrixXd& hyperplane){
+                Eigen::VectorXd projection = hyperplane * point;
+                HashKey hash;
+                for (int i = 0; 1 < projection.size(); i++){
+                    hash.push_back(projection(i) > 0 ? 1 : 0);
+                }
+                return hash;
+            }
+
+            std::vector<HashKey> generateHash(const Eigen::VectorXd& point) {
+                std::cout << "[GENERATE_HASH] Generating hash for point " << point.transpose() << std::endl;
+                std::vector<HashKey> hashes;
+                for (int i = 0; i < numTables; i++) {
+                    HashKey h = hashFunction(point, hyperplanes[i]);
+                    std::cout << "  [TABLE " << i << "] hash=(";
+                    for (int bit : h) std::cout << bit;
+                    std::cout << ")" << std::endl;
+                    hashes.push_back(h);
+                }
+                return hashes;
+            }
+
+            void add(const Eigen::VectorXd& point, int id) {
+                std::cout << "[ADD] Adding point " << id << ": " << point.transpose() << std::endl;
+                std::vector<HashKey> hashes = generateHash(point);
+                for (int i = 0; i < numTables; i++) {
+                    tables[i][hashes[i]].push_back(id);
+                    std::cout << "  [TABLE " << i << "] Inserted " << id << " into bucket (";
+                    for (int bit : hashes[i]) std::cout << bit;
+                    std::cout << ")" << std::endl;
+                }
+            }
+
+            std::vector<int> query(const Eigen::VectorXd& queryPoint) {
+                std::cout << "[QUERY] Querying for point " << queryPoint.transpose() << std::endl;
+                std::set<int> candidateIds;
+                std::vector<HashKey> hashes = generateHash(queryPoint);
+
+                for (int i = 0; i < numTables; i++) {
+                    auto it = tables[i].find(hashes[i]);
+                    if (it != tables[i].end()) {
+                        std::cout << "  [TABLE " << i << "] Bicket contains: ";
+                        for (int id : it -> second) {
+                            std::cout << id << " ";
+                            candidateIds.insert(id);
+                        }
+                        std::cout << std::endl;
+                    } else {
+                        std::cout << "  [TABLE " << i << "] Bucket is empty" << std::endl;
+                    }
+                }
+
+                std::cout << "[QUERY_RESULT] Candidates found: ";
+                for (int id : candidateIds) std::cout << id << " ";
+                std::cout << std::endl;
+
+                return std::vector<int>(candidateIds.begin(), candidateIds.end());
+            }
     };
 }
